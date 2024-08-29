07feb82137dbb9bb0187a816d32f3e38f1b5243c
FaisalYousuf1 <fyousuf@unomaha.edu>
2024-08-29 01:15:02 +0000

    Finished Lab 1

--- MadLib.py
+++ MadLib.py
@@ -1,13 +1,18 @@
 #MadLib.py
-#Name:
+#Name:    
 #Date:
 #Assignment:
 
 def main():
   print("Madlib")
   #Ask user for words
-
+  Place = input ("Enter a place: ")
+  Adjective = input ("Enter an adjective: ")
+  Noun = input ("Enter a noun: ")
   #Print the story with the user supplied words.
+
+  print("I am headed to(the) " + Place + " to spend time with my " + Adjective + " " + Noun)
+
 
 
 
